32f6046944618c9bdf0067cdcc5115f099efe1a3
Pavel Mikryukov <pavelmikryukov2001@gmail.com>
2026-09-04 15:33:50 +0400

    Добавление DAO

--- src/DataAccess/Data.java
+++ src/DataAccess/Data.java
@@ -1,28 +1,28 @@
 package DataAccess;
 
-import Domain.Models.RealtyDto;
+import DataAccess.Realty.RealtyDao;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 public final class Data {
 
-    private static final ArrayList<RealtyDto> realty = new ArrayList<>();
+    private static final ArrayList<RealtyDao> realty = new ArrayList<>();
 
     /**
      * Получить коллекцию данных о недвижимости.
      * @return Коллекция данных о недвижимости.
      */
-    public static Collection<RealtyDto> getRealty(){
+    public static Collection<RealtyDao> getRealty(){
         return realty;
     }
 
     /**
      * Добавить коллекцию информации о недвижимостях.
-     * @param realtyDtos Коллекция информации о недвижимостях.
+     * @param realtyDaos Коллекция информации о недвижимостях.
      * @return true - если добавление успешно, иначе false.
      */
-    public static boolean addRealty(Collection<RealtyDto> realtyDtos){
-        return realty.addAll(realtyDtos);
+    public static boolean addRealty(Collection<RealtyDao> realtyDaos){
+        return realty.addAll(realtyDaos);
     }
 }
