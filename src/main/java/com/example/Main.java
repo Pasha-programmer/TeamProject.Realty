83452e3621ff5376391b158edc4ccd45cb891e00
M. V. Novikov <m.v.novikov88@gmail.com>
2026-09-08 16:11:36 +0300

    code review fixes

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,10 +4,6 @@ import com.example.ConsoleUI.Menu.MainMenu;
 import com.example.Domain.Models.RealtyDto;
 import com.example.Infrastructure.Services.Realty.RealtyGetterService;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.example.DataAccess.Realty.RealtyDao;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -15,18 +11,5 @@ public class Main {
                 new RealtyGetterService()
         );
         mainMenu.run();
-
-//        ObjectMapper mapper = new ObjectMapper();
-//        try {
-//            RealtyDao house = mapper.readValue("{\n" +
-//                    "    \"address\": \"3948 Adams Rd, Fairview, 29481\",\n" +
-//                    "    \"cost\": 629104,\n" +
-//                    "    \"totalArea\": 218.4\n" +
-//                    "  },\n", RealtyDao.class);
-//            System.out.println(house.address + " - " + house.totalArea);
-//
-//        } catch (JsonProcessingException e) {
-//            e.printStackTrace();
-//        }
     }
 }
