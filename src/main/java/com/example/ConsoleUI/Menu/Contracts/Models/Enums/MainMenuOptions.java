4407eb7de337ae3e39a5df6a39468acfd3758c72
Pavel Mikryukov <pavelmikryukov2001@gmail.com>
2026-09-17 13:08:27 +0400

    фикс

--- src/main/java/com/example/ConsoleUI/Menu/Contracts/Models/Enums/MainMenuOptions.java
+++ src/main/java/com/example/ConsoleUI/Menu/Contracts/Models/Enums/MainMenuOptions.java
@@ -8,9 +8,8 @@ public enum MainMenuOptions implements EnumWithNumber {
     SortingData(2),
     ShowData(3),
     Search(4),
-    ImportData(5),
-    ExportData(6),
-    Exit(7);
+    ExportData(5),
+    Exit(6);
 
     private final int value;
 
